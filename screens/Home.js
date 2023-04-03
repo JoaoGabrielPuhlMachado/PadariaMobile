15c174dc223c05d731a52f3f3f8cc84c58f71e9f
joaogabriel <zala4453523@gmail.com>
2023-04-03 15:07:37 -0300

    V1

---
+++ screens/Home.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { Button, Card } from 'react-native-paper';
+import { DefaultTheme } from 'react-native-paper';
+
+export default function Home ({navigation}) {
+  return  (
+    <ScrollView style={styles.scrollView}>
+      <Card style={styles.card}>
+        <Card.Content>
+          <Button mode="contained" onPress={() => navigation.navigate('Livros')}>
+            Livros 
+          </Button>
+        </Card.Content>
+      </Card>
+      <Card style={styles.card}>
+        <Card.Content>
+          <Button mode="contained" onPress={() => navigation.navigate('Filmes')}>
+            Filmes 
+          </Button>
+        </Card.Content>
+      </Card>
+      <Card style={styles.card}>
+        <Card.Content>
+          <Button mode="contained" onPress={() => navigation.navigate('Series')}>
+            Séries 
+          </Button>
+        </Card.Content>
+      </Card>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: DefaultTheme.colors.background,
+    paddingTop: 10
+  },
+  card: {
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
+});
